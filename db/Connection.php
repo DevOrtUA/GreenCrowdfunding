13c057593c94b7a06277edb79b5aa73b4bee80ce
DevOrtUA <57683132+DevOrtUA@users.noreply.github.com>
2021-05-23 18:01:02 +0200

    fix config

--- db/Connection.php
+++ db/Connection.php
@@ -5,9 +5,9 @@ class Connection
     public static function Connect()
     {
         define('server', 'eu-cdbr-west-01.cleardb.net');
-        define('db', 'crowdfunding');
+        define('db', 'heroku_3bdf0e5b7aa1dbf');
         define('username', 'b2f98f70fdf170');
-        define('password', '51daa4fe1e2b6ea ');
+        define('password', '51daa4fe1e2b6ea');
         $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
         try {
             $connection = new PDO("mysql:host=" . server . ";dbname=" . db, username, password, $options);
