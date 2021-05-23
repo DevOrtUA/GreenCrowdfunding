f8fa842700df3d6523698b2a7138e2e1355546d3
DevOrtUA <57683132+DevOrtUA@users.noreply.github.com>
2021-05-23 17:51:08 +0200

    Add database config

--- db/Connection.php
+++ db/Connection.php
@@ -4,10 +4,10 @@ class Connection
 {
     public static function Connect()
     {
-        define('server', 'localhost');
+        define('server', 'eu-cdbr-west-02.cleardb.net');
         define('db', 'crowdfunding');
-        define('username', 'admin');
-        define('password', 'admin_2020@');
+        define('username', 'b2f98f70fdf170');
+        define('password', '51daa4fe1e2b6ea ');
         $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
         try {
             $connection = new PDO("mysql:host=" . server . ";dbname=" . db, username, password, $options);
